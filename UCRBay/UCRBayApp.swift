8407835147298d55fd1f3adc90747f6781898276
Adrian Mtz <amart654@ucr.edu>
2024-02-15 16:28:08 -0800

    Initial Commit

---
+++ UCRBay/UCRBayApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCRBayApp.swift
+//  UCRBay
+//
+//  Created by Adrian Mtz on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct UCRBayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
