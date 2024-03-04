b43514454bad2899bc58d24bb62543270b0f92ce
junjie yu <94166105+emptybubble@users.noreply.github.com>
2024-03-03 23:44:14 -0800

    Create InboxView.swift by jiale

---
+++ UCRBay/InboxView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct InboxView: View {
+    var body: some View {
+        Text("Inbox")
+    }
+}
+
+struct InboxView_Previews: PreviewProvider {
+    static var previews: some View {
+        InboxView()
+    }
+}
