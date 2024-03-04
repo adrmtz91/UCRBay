0b6084d30371bd9d690dc0c990bc4f1964950fc2
junjie yu <94166105+emptybubble@users.noreply.github.com>
2024-03-03 23:45:39 -0800

    Create MyitemsView.swift by jiale

---
+++ UCRBay/MyitemsView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct MyItemsView: View {
+    var body: some View {
+        Text("My Items")
+    }
+}
+
+struct MyItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyItemsView()
+    }
+}
