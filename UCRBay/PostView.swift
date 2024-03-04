fa6e6098575a209b9a5e5cb5d506b5d887639685
junjie yu <94166105+emptybubble@users.noreply.github.com>
2024-03-03 23:45:01 -0800

    Create PostView.swift by jiale

---
+++ UCRBay/PostView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text("Post")
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+    }
+}
