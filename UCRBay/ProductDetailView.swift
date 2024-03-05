db7442ce9f8151b87375f4623748e81a7d4a7941
junjie yu <94166105+emptybubble@users.noreply.github.com>
2024-03-05 01:14:48 -0800

    Create ProductDetailView.swift

---
+++ UCRBay/ProductDetailView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct ProductDetailView: View {
+    var product: Product
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            if let imageName = product.imageName {
+                Image(imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.bottom, 2)
+            }
+            
+            Text(product.name)
+                .font(.title)
+                .padding(.bottom, 2)
+            
+            Text(product.price)
+                .font(.headline)
+                .foregroundColor(.secondary)
+                .padding(.bottom, 2)
+            
+            if let description = product.description {
+                Text(description)
+                    .font(.body)
+                    .padding(.bottom, 2)
+            }
+            
+            Text("Category: \(product.category)")
+                .font(.caption)
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .navigationTitle(product.name)
+    }
+}
